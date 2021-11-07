aa43152403ada3891c405a1c60693369a8a2e590
josempita <josempita@gmail.com>
2021-11-07 10:03:13 +0200

    Using new lora library

--- beekeepa/beekeeparadiohead/beekeeparadiohead.ino
+++ beekeepa/beekeeparadiohead/beekeeparadiohead.ino
@@ -7,16 +7,18 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "Audio.h"
+#include "FS.h"
 
 // SD SPI pins can be chosen freely, but cannot overlap with other ports!
 #define SD_CS 23
 #define SD_SCK 17
 #define SD_MOSI 12
-#define SD_MISO 13
+#define SD_MISO 21
 
 #define LORA_FREQ 433.0
 
-#define LOG_PATH "lora_recv.log"
+#define LOG_PATH "/lora_recv.log"
 
 // The sd card can also use a virtual SPI bus
 SPIClass sd_spi(HSPI);
@@ -32,11 +34,24 @@ RH_RF95 rf95(LORA_CS, LORA_IRQ, sx1278_spi);
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+//AUDIO
+#define I2S_DOUT      25
+#define I2S_BCLK      33    // I2S
+#define I2S_LRC       32
+#define AUDIO_EN      02
+volatile bool beeson = false;
+Audio audio;
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
 void setup() {
+    
+    // Serial output
+    Serial.begin(115200);
     pinMode(SD_CS, OUTPUT);
     pinMode(LORA_CS, OUTPUT);
+    pinMode(AUDIO_EN, OUTPUT);
+    digitalWrite(AUDIO_EN, LOW);
     digitalWrite(LORA_CS, HIGH);
     digitalWrite(SD_CS, HIGH);  
       //reset OLED display via software
@@ -60,10 +75,6 @@ void setup() {
 
   
     // Builtin LED
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    // Serial output
-    Serial.begin(115200);
 
     // LoRa: Init
     pinMode(LORA_RST, OUTPUT);
@@ -89,12 +100,25 @@ void setup() {
 
     // LoRa: Set max (23 dbm) transmission power. 
     rf95.setTxPower(23, false);
-    delay(2000);
+    delay(4000);
     display.clearDisplay();
     display.setCursor(0,10);
     display.print("Checking SD CARD");
     display.display();  
     delay(1000);
+
+        pinMode(LED_BUILTIN, OUTPUT);
+    String lorapins = "CS:";
+    lorapins.concat(LORA_CS);
+    lorapins.concat("MISO:");
+    lorapins.concat(LORA_MISO);
+    lorapins.concat("MOSI:");
+    lorapins.concat(LORA_MOSI);
+    lorapins.concat("SCK:");
+    lorapins.concat(LORA_SCK);
+    lorapins.concat("IRQ:");
+    lorapins.concat(LORA_IRQ);
+    Serial.println(lorapins);
     
     // SD Card
     sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
@@ -108,11 +132,21 @@ void setup() {
     }
     else {
         display.setCursor(0,10);
-        display.print("SD Card: mounting failed.");
+        display.print("SD Card: mounted.");
         Serial.println("SD Card: mounted.");
              display.display();  
     }
-   delay(2000);
+    
+   //AUDIO 
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(21); // 0...21
+  audio.connecttoFS(SD,"/beehive.mp3");
+  audio.setFileLoop(true);
+  display.clearDisplay();
+  display.setCursor(0,10);
+  display.print("Audio Configured");
+  Serial.println("Audio Configured");
+  display.display();  
 }
 
 uint8_t lora_buf[RH_RF95_MAX_MESSAGE_LEN];
@@ -121,7 +155,7 @@ uint8_t receive_counter = 0;
 
 void loop() {
     // Blink LED
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
     lora_len = RH_RF95_MAX_MESSAGE_LEN;
     if (rf95.recv(lora_buf, &lora_len)) {
@@ -134,18 +168,36 @@ void loop() {
          display.print("Received packet");
          display.setCursor(0,30);
          display.printf("%s", lora_buf);
-         display.display();  
-         
-        File test = SD.open(LOG_PATH, FILE_APPEND);
-        if (!test) {
-            Serial.println("SD Card: writing file failed.");
-        } else {
-            Serial.printf("SD Card: appending data to %s.\n", LOG_PATH);
-            test.write(lora_buf, lora_len);
-            test.printf("\n\n");
-            test.close();
-        }
+         display.display(); 
+           
+         String dataReceived = (char*)lora_buf;
+          if(dataReceived == "Hello World5"){
+            beeson = true;
+          }
+          if(dataReceived == "Hello World10"){
+            beeson = false;
+          }
+
+        // Writing to file works but disabled when playing audio 
+//        File test = SD.open(LOG_PATH, FILE_APPEND);
+//        if (!test) {
+//            Serial.println("SD Card: writing file failed.");
+//        } else {
+//            Serial.printf("SD Card: appending data to %s.\n", LOG_PATH);
+//            test.write(lora_buf, lora_len);
+//            test.printf("\n\n");++
+//            test.close();
+//        }
+
+          
+    }
+    if (beeson == true){
+        Serial.println("Bees on");
+        digitalWrite(AUDIO_EN, HIGH);
+        audio.loop();                
+    }else {
+        digitalWrite(AUDIO_EN, LOW);
     }
 
-    delay(100);
+    //delay(100);
 }
